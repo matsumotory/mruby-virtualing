34e9c9b88573f061f94588cc03a080f49b301fa9
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-05-15 11:57:49 +0900

    if don't use bind config, ignore bind option on jainling

--- mrblib/virtual.rb
+++ mrblib/virtual.rb
@@ -25,7 +25,7 @@ class Virtual
   def setup_chroot config
     # TODO: implement to mruby-jailing
     path = config[:path] ? config[:path] : "jailing"
-    bind_cmd = config[:bind].map {|dir| "--bind #{dir}" }.join(" ")
+    bind_cmd = config[:bind].map {|dir| "--bind #{dir}" }.join(" ") if config[:bind]
     run_cmd = "#{path} --root=#{config[:root]} #{bind_cmd} -- #{config[:cmnd]}"
     if system(run_cmd)
       raise "setup chroot failed"
